e608233fd3016fd82046c9e3e0f708f46cee63db
kaif28 <40994365+kaif28@users.noreply.github.com>
2018-08-09 15:10:47 +0530

    Add files via upload

---
+++ kaif/Innerclass.java
@@ -0,0 +1,15 @@
+
+package innerclass;
+
+public class Innerclass {
+ int data=30;  
+ class Inner{  
+  void msg(){System.out.println("data is "+data);}  
+ }  
+ public static void main(String args[]){  
+  Innerclass  obj=new Innerclass();  
+  Innerclass.Inner in=obj.new Inner();  
+  in.msg(); 
+   }
+    
+}
